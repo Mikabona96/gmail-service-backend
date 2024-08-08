1d40895df5e05ed789bf6f4b72652f8caa7348d3
Dmytro Holub <dmitriygolubapk@gmail.com>
2024-08-08 16:29:35 +0300

    Message read/unread/ spam /trash

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -19,6 +19,103 @@ export class MessagesService {
     'CATEGORY_UPDATES',
     'CATEGORY_FORUMS',
   ];
+
+  getOneMessage = async ({
+    id,
+    access_token,
+  }: {
+    id: string;
+    access_token: string;
+  }) => {
+    const decodeBase64 = (encoded = '') => {
+      const decodedBuffer = Buffer.from(encoded, 'base64');
+      return decodedBuffer.toString('utf-8');
+    };
+    const auth = new google.auth.OAuth2();
+    auth.setCredentials({ access_token });
+
+    const gmail = google.gmail({ version: 'v1', auth });
+    const messageRes = await gmail.users.messages.get({
+      userId: 'me',
+      id: id,
+    });
+    const messageResData = messageRes.data;
+
+    const payload = messageRes.data.payload;
+
+    if (!payload) {
+      console.log('No payload for this message!');
+      return;
+    }
+
+    //+ Modified headers ==================Start================
+    const modifiedHeaders = messageRes.data.payload.headers.reduce(
+      (acc, next) => {
+        if (
+          next.name === 'Subject' ||
+          next.name === 'From' ||
+          next.name === 'To' ||
+          next.name === 'Date'
+        ) {
+          acc[next.name] = next.value;
+        }
+        return acc;
+      },
+      {},
+    );
+
+    //+ Modified headers ==================End==================
+
+    let message = '';
+
+    if (payload.body && payload.body.data) {
+      message += decodeBase64(payload.body.data);
+    }
+
+    //$ Combine parts of message ================Start===============
+    const parts = payload.parts || [];
+    const attachments = [];
+
+    parts.forEach((part) => {
+      if (part.body && part.body.attachmentId) {
+        attachments.push({
+          partId: part.partId,
+          filename: part.filename,
+          headers: part.headers,
+          attachment: part.body.attachmentId,
+          size: part.body.size,
+        });
+      }
+      if (part.parts) {
+        part.parts.forEach((p) => {
+          message += decodeBase64(p.body.data);
+        });
+      }
+      message += decodeBase64(part.body.data);
+    });
+
+    //$ Combine parts of message ================End================
+    let htmlPage = '';
+    if (message.split('<html')[1]) {
+      htmlPage = '<html ' + message.split('<html')[1];
+    }
+
+    const returnedObject = {
+      id: messageResData.id,
+      threadId: messageResData.threadId,
+      labelIds: messageResData.labelIds,
+      snippet: messageResData.snippet.trim(),
+      headers: modifiedHeaders,
+      sizeEstimate: messageResData.sizeEstimate,
+      historyId: messageResData.historyId,
+      internalDate: messageResData.internalDate,
+      htmlPage,
+      attachments,
+    };
+
+    return returnedObject;
+  };
+
   async getMessages(
     accessToken: string,
     { labelIds, pageToken }: { labelIds?: string[]; pageToken?: string },
@@ -33,93 +130,12 @@ export class MessagesService {
       labelIds,
     });
 
-    const decodeBase64 = (encoded = '') => {
-      const decodedBuffer = Buffer.from(encoded, 'base64');
-      return decodedBuffer.toString('utf-8');
-    };
-
     //* Get message's text
     const messagePromises = res.data.messages.map(async (msg) => {
-      const messageRes = await gmail.users.messages.get({
-        userId: 'me',
+      return await this.getOneMessage({
+        access_token: accessToken,
         id: msg.id,
       });
-      const messageResData = messageRes.data;
-
-      const payload = messageRes.data.payload;
-
-      if (!payload) {
-        console.log('No payload for this message!');
-        return;
-      }
-
-      //+ Modified headers ==================Start================
-      const modifiedHeaders = messageRes.data.payload.headers.reduce(
-        (acc, next) => {
-          if (
-            next.name === 'Subject' ||
-            next.name === 'From' ||
-            next.name === 'To' ||
-            next.name === 'Date'
-          ) {
-            acc[next.name] = next.value;
-          }
-          return acc;
-        },
-        {},
-      );
-
-      //+ Modified headers ==================End==================
-
-      let message = '';
-
-      if (payload.body && payload.body.data) {
-        message += decodeBase64(payload.body.data);
-      }
-
-      //$ Combine parts of message ================Start===============
-      const parts = payload.parts || [];
-      const attachments = [];
-
-      parts.forEach((part) => {
-        if (part.body && part.body.attachmentId) {
-          attachments.push({
-            partId: part.partId,
-            filename: part.filename,
-            headers: part.headers,
-            attachment: part.body.attachmentId,
-            size: part.body.size,
-          });
-        }
-        if (part.parts) {
-          part.parts.forEach((p) => {
-            message += decodeBase64(p.body.data);
-          });
-        }
-        message += decodeBase64(part.body.data);
-      });
-
-      //$ Combine parts of message ================End================
-      let htmlPage = '';
-      if (message.split('<html')[1]) {
-        htmlPage = '<html ' + message.split('<html')[1];
-      }
-
-      const returnedObject = {
-        id: messageResData.id,
-        threadId: messageResData.threadId,
-        labelIds: messageResData.labelIds,
-        snippet: messageResData.snippet.trim(),
-        headers: modifiedHeaders,
-        sizeEstimate: messageResData.sizeEstimate,
-        historyId: messageResData.historyId,
-        internalDate: messageResData.internalDate,
-        htmlPage,
-        attachments,
-      };
-
-      // return { ...messageResData, htmlPage, attachments };
-      return returnedObject;
     });
 
     const resolvedMessages = await Promise.all(messagePromises);
@@ -186,20 +202,30 @@ export class MessagesService {
       throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
     }
   }
-  async getMessage(access_token: string, id: string) {
+  async markAsRead(access_token: string, id: string) {
     const auth = new google.auth.OAuth2();
     auth.setCredentials({ access_token: access_token });
 
-    console.log('getMessage....');
-
     const gmail = google.gmail({ version: 'v1', auth });
     try {
-      const res = await gmail.users.messages.get({
+      const res = await gmail.users.messages.modify({
         userId: 'me',
         id,
+        requestBody: {
+          addLabelIds: [],
+          removeLabelIds: ['UNREAD'],
+        },
       });
-      console.log('🚀 ~ MessagesService ~ getMessage ~ res:', res.data);
       return res.data;
+    } catch (error) {
+      throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+    }
+  }
+  async getMessage(access_token: string, id: string) {
+    try {
+      await this.markAsRead(access_token, id);
+      const res = await this.getOneMessage({ id, access_token });
+      return res;
     } catch (error) {
       throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
     }
